f8e0a52793ab39754e1e7e92c4340e01ae1767df
Chase Szafranski <70776692+chasesza@users.noreply.github.com>
2021-01-23 13:38:16 -0500

    Update chess_program.py

--- chess_program.py
+++ chess_program.py
@@ -2,9 +2,9 @@ import berserk
 import serial
 from datetime import datetime
 
-UART_OFFSET = 33;
+UART_OFFSET = 33
+USER_LICHESS_ID = 'chaseszafranski'
 pgn_path = '/mnt/c/users/chase/dropbox/chesspgns/'
-
 ser = serial.Serial('/dev/ttyS4')
 
 def wait_for_game_start(stream):
@@ -47,7 +47,6 @@ def get_next_game_state(move_list, id, stream):
             state = next(stream)
         else:
             state = get_next_game_state(get_move_list(state), id, stream)
-        #print(state)
     return state
 
 
@@ -77,13 +76,12 @@ def get_move():
     :return: the four character move
     :rtype: string
     """
-    #return input("move:  ")  # this needs to be changed for the actual board
     ser.timeout = 0.1
     start_byte = 'n'
     move_read = 'n'
     while start_byte != 's':
         start_byte = ser.read().decode('ascii')
-    ser.write(b'a')  # acknowledge that we are waiting for transmission
+    ser.write(b'a')  # acknowledge that the pc is ready to receive
     while start_byte != '':
         start_byte = ser.read().decode('ascii')
     while len(move_read) != 2:
@@ -113,7 +111,6 @@ def make_move(game, next_move, list_of_moves, stream):
     if next_move[0] == next_move[2] and next_move[1] == next_move[3] and (next_move[1] == '2' or next_move[1] == '7'):
         offer_draw = True  # offer a draw
         next_move = get_move()
-        # next_move = input("Draw offer queued\nmove:  ")
     try:
         client.board.make_move(game, next_move)
     except berserk.exceptions.ResponseError:
@@ -145,7 +142,6 @@ def set_clock(state, who):
     :param string who: "black" or "white"
     :return: none
     """
-    # state[key] is a datetime.datetime
     key = who[0] + "time"
     print(str(who) + ": " + str(state[key].minute) + ":" + str(state[key].second))  # this will need to be changed
 
@@ -156,7 +152,6 @@ def display_last_move(state):
     :param dict state: the current game state
     :return: none
     """
-    #print(state["moves"].split()[-1])  # this will need to be changed
     prev_move = state["moves"].split()[-1]
     ser.write(b'm')
     ascii_move_int = (ord(prev_move[1])-ord('1'))*8
@@ -203,15 +198,12 @@ def handle_draw(id):
     :return: whether the draw was accepted
     :rtype: boolean
     """
-    # decision = input("A draw has been offered (y/n):  ")
     ser.write(b'd')
     decis = get_move()  # decision
     if decis[0] == decis[2] and decis[1] == decis[3] and (decis[1] == '2' or decis[1] == '7'):
-        # print("y was entered")
         client.board.accept_draw(id)
         return True
     else:
-        # print("y was not entered")
         client.board.decline_draw(id)
         return False
 
@@ -226,7 +218,6 @@ def game_over(state, stream):
     """
     while state["type"] != "gameState":
         state = next(stream)
-    #print(state)
     if state["status"] == "started":
         return False
     winner = state.get("winner")
@@ -253,16 +244,10 @@ event_stream = client.board.stream_incoming_events()
 
 game_ended = False
 
-#play = input("Do you want to start a game (y/n)?  ")
+# Wait for input to start a game
+# double clicking the same button indicates that the user wants to play
 play = get_move()
 while play[0] == play[2] and play[1] == play[3]:
-    # Ignore stream events until current event is a challenge, then get challengeId and start the game
-    # This will need to be replaced with seek code for the real thing
-    # current_event = next(event_stream)
-    # while current_event["type"] != "challenge":
-        # current_event = next(event_stream)
-    # challengeID = current_event["challenge"]["id"]
-    # client.challenges.accept(challengeID)
 
     display_two_squares('d1e8')
 
@@ -272,10 +257,8 @@ while play[0] == play[2] and play[1] == play[3]:
     print("gameID:  " + gameID)
     game_stream = client.board.stream_game_state(gameID)
 
-    # Should be the gameFull data. If not, idk what happened
     game_event = next(game_stream)
-
-    if game_event["white"]["id"] == "chaseszafranski":
+    if game_event["white"]["id"] == USER_LICHESS_ID:
         color = "white"
         their_color = "black"
         display_two_squares('c2f1')
@@ -287,39 +270,35 @@ while play[0] == play[2] and play[1] == play[3]:
     print("color:  " + color)
 
     game_state = game_event["state"]
-    # print("game state:")
-    # print(game_state)
 
     move_number = 1
     move = ""
     game_ended = False
     while move != "quit":
+        # user's move
         if move_number > 1 or color == "white":
             move = get_move();
             target_move_list = make_move(gameID, move, get_move_list(game_state), game_stream)
             if game_ended:
                 break
-            # this function is different to avoid issues with draw offers opponents place during the user's move
+            # this function is different to avoid issues with draw offers opponents placed during the user's move
             game_state = get_post_move_game_state(target_move_list, game_stream)  # state after user makes their move
             if game_over(game_state, game_stream):
                 break
             set_clock(game_state, their_color)
             display_last_move(game_state)  # user move
+        # opponent's move
         game_state = get_next_game_state(get_move_list(game_state), gameID, game_stream)  # state after opponent moves
         if game_over(game_state, game_stream):
             break
         set_clock(game_state, color)
         display_last_move(game_state)  # opponent's move
-        print('\a')  # beep sound after opponent's move, remove for final
-        # print("\n\n")  # spacing for full game state
-        # print(game_state)
-        # print("\n\n")  # spacing for full game state
+        print('\a')  # notification sound after opponent's move
         move_number += 1
 
     game_file = open(pgn_path+str(datetime.now())+'.txt','w')  # create game file
-    game_file.write(client.games.export(gameID, True))  # write the game to the file as pgn
+    game_file.write(client.games.export(gameID, True))  # write the game to the file as a pgn
     game_file.close()  # close the game file
-    # play = input("\nPlay again (y/n)?  ")
     play = get_move()
 ser.write(b'o')
-x = input("press enter to exit")
+input("press enter to exit")
